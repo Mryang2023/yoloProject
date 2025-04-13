2e2ee0424f9f58813e090a2fc210268858e60cf7
admin <1340774602@qq.com>
2025-04-13 19:34:02 +0800

    feat: 训练结果

--- yolo11/train.py
+++ yolo11/train.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     yolo = YOLO('yolo11n.yaml')
-    yolo.train(data='dataset.yaml', epochs=100, batch=3, device='cpu')
+    yolo.train(data='dataset.yaml', epochs=100, batch=3, device='cuda')
